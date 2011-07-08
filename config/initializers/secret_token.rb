60a623d93d5180aca4f71e6f99d4aba16a214ff1
Kaio Max <kaiomaks@gmail.com>
2011-07-08 10:01:41 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Editais::Application.config.secret_token = 'a353e08af7f45d210d066eb19b5d89dc1be5a6cff6c56d1a7f647602ed110b01f388041ee2f21ac015149f9fdf97cdc813c9f5b5db0bde5251a36b62022932df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Editais::Application.config.session_store :cookie_store, :key => '_editais_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Editais::Application.config.session_store :active_record_store
