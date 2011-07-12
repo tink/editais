af6238ec588250ba928e75b7948777af7891a388
Paulo Fagiani <pfagiani@gmail.com>
2011-07-11 23:17:06 -0300

    corrigindo erro de digitação

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :s3
     config.s3_access_key_id = ENV['s3_access_key_id']
-    config.s3.secret_access_key = ENV['s3_secret_access_key']
+    config.s3_secret_access_key = ENV['s3_secret_access_key']
     config.s3_bucket = ENV['s3_bucket']
   else
     config.storage = :file
