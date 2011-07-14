69640a70f63dcc78b9b7997def55774e4e9060d6
Anderson Dias <andersondaraujo@gmail.com>
2011-07-14 11:10:11 -0300

    Mostrando quantos caracteres faltam no resumo

--- public/javascripts/editais/form.js
+++ public/javascripts/editais/form.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  $('#edital_resumo').limit(250, '#charsLeft');
+
   $('#edital_site').bind('click', function(e){
     if( $.trim( $(e.target).val() ) == '' ) {
       $(e.target).val('http://');
