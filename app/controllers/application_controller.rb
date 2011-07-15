3f0eb5c4cb6ef9296779ed5391bd3bf4e759769a
Anderson Dias <andersondaraujo@gmail.com>
2011-07-15 16:09:59 -0300

    Nova página de 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,19 @@ class ApplicationController < ActionController::Base
   helper_method :current_empresa
 
   protect_from_forgery
+
+  protected
   def current_empresa
     if session[:empresa_id]
       @empresa ||= Empresa.find(session[:empresa_id])
     end
+  end
+
+  def render_404
+    respond_to do |type| 
+      type.html { render :template => "errors/404", :layout => 'base', :status => 404 } 
+      type.all  { render :nothing => true, :status => 404 } 
+    end
+    true
   end
 end
