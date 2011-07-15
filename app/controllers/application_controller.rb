3fded6c8ed2657855dacf403df1ff4ff1270f7d9
Anderson Dias <andersondaraujo@gmail.com>
2011-07-15 16:37:01 -0300

    Desfazendo tentativas sem sucesso

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordInvalid, :with => :render_404
-  rescue_from ActionController::RoutingError, :with => :render_404
-  rescue_from ActionController::UnknownAction, :with => :render_404
-
   helper_method :current_empresa
 
   protect_from_forgery
@@ -12,13 +8,5 @@ class ApplicationController < ActionController::Base
     if session[:empresa_id]
       @empresa ||= Empresa.find(session[:empresa_id])
     end
-  end
-
-  def render_404
-    respond_to do |type| 
-      type.html { render :template => "errors/404", :layout => 'base', :status => 404 } 
-      type.all  { render :nothing => true, :status => 404 } 
-    end
-    true
   end
 end
